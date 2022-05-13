739011f24c2006b42694b4f5144e7f8fc80f5e62
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-13 18:37:40 +0900

    新規登録カナ文字制限&不要コメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+         validates :nickname, presence: true
+
          with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
           validates :first_name
           validates :second_name
          end
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :second_name, presence: true
+
+         with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを使用してください' } do
          validates :first_name_kana, presence: true
          validates :second_name_kana, presence: true
+         end
          validates :birthday, presence: true
 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
