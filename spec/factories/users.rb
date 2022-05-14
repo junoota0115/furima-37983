bdccb723ef961952a33ae181272b49ab4d84bbf2
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-14 11:59:56 +0900

    テストコード書き加え

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
+    nickname              { 'テスト用ユーザー' }
+    email                 { Faker::Internet.free_email }
     password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    password_confirmation { password }
+    first_name           { '佐藤' }
+    second_name            { '太郎' }
+    first_name_kana      { 'サトウ' }
+    second_name_kana        { 'タロウ' }
+    birthday              { '1970-01-01' }
   end
 end
