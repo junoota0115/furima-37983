4806c08b21848fc8c4a411027f08b45a6d839c03
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-16 15:57:00 +0900

    送料、都道府県、配送日のアクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,19 @@ class Item < ApplicationRecord
 
 
   belongs_to :user
-
-
-   #has_one_attached :image　後でチェック
+  has_one_attached :image
 
   validates :item_name, presence: true
   validates :content, presence: true
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :send_day
+
   validates :category_id, presence: true
   validates :item_status_id, presence: true
   validates :postage_id , presence: true
@@ -15,12 +22,10 @@ class Item < ApplicationRecord
   validates :send_day_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :item_status
      #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 , message:"can't be blank"} 
   validates :item_status_id, numericality: { other_than: 1 , message:"can't be blank"}
-
+  validates :postage_id, numericality: { other_than: 1 , message:"can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 0 , message:"can't be blank"}
+  validates :send_day_id, numericality: { other_than: 1 , message:"can't be blank"}
 end
