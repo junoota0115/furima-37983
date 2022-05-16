3fdf816d1345512ec150767ae1d2d0a509de66a7
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-16 11:08:15 +0900

    [クリエイト]ルーティング、コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,20 @@
-class ItemsController < ApplicationController
-  
+class ItemsController < ApplicationController  
+
 def new
+  @item = Item.new
 end
 
+def create
+  @item  = Item.create(item_params)
+if @item.save
+  redirect_to root_path(@item)
+else
+  render :new
+end
+end
+
+private
+def item_params
+  params.require(:item).permit(:item_name, :content, :category_id,:item_status_id,:postage_id,:prefecture_id,:send_day_id,:price,:user)
+end
 end
