544cbbe2be59fb0b23f314644876a34b3d27f980
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-16 15:02:57 +0900

    カテゴリー、商品状態のアクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
 
+
   belongs_to :user
+
+
+   #has_one_attached :image　後でチェック
 
   validates :item_name, presence: true
   validates :content, presence: true
@@ -11,4 +15,12 @@ class Item < ApplicationRecord
   validates :send_day_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+     #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message:"can't be blank"} 
+  validates :item_status_id, numericality: { other_than: 1 , message:"can't be blank"}
+
 end
