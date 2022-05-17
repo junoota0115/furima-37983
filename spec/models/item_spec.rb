1b331054fae81972f0ef05c777650cb9adbe0755
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-17 19:44:31 +0900

    テストコード　price異常系追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -85,6 +85,12 @@ describe '商品出品' do
       expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
     end
 
+    it '価格が半角数字でないと登録できない' do
+      @item.price =  "あああ"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+
     it 'imageが空だと登録できない' do
       @item.image = nil  
     @item.valid?
