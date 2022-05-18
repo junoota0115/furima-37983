cecf2c78718609728e80001aa1bd852ca0c12465
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-18 22:03:00 +0900

    ルーティング、コントローラー、HTML編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController  
   before_action :authenticate_user!, except: [:index,:show]
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -21,9 +22,30 @@ def show
   @item  = Item.find(params[:id])
 end
 
+def edit
+  @item  = Item.find(params[:id])
+  if @item.user_id == current_user.id && @item.present?
+  else
+    redirect_to root_path
+  end
+end
+  
+
+
+def update
+  @item = Item.find(params[:id])
+  @item.update(item_params)
+  if @item.valid?
+    redirect_to item_path(item_params)
+  else
+    render :edit
+  end
+  
+end
+
+
 private
 def item_params
   params.require(:item).permit(:image,:item_name, :content, :category_id,:item_status_id,:postage_id,:prefecture_id,:send_day_id,:price,:user).merge(user_id: current_user.id)
 end
-
 end
