533762027c5c6c300e75136ebb9cc1ebec087683
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-18 12:15:32 +0900

    商品名、販売価格、画像の表示、何もない場合はダミーを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController  
   before_action :authenticate_user!, except: [:index]
   def index
-   # @items = Item.order("created_at DESC")
+    @items = Item.all
+    #@items = Item.includes(:user).order("created_at DESC")
   end
 
 def new
