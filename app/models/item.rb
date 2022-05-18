6a3711cfb7f9958d389c85414f42e9014dee2add
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-18 10:39:13 +0900

    presence 重複の為削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,7 @@ class Item < ApplicationRecord
   validates :content, presence: true
   validates :image, presence: true
   with_options presence:true,format: {with: /\A[0-9]+\z/ }do
-  validates :price, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-  presence:{message: "can't be blank"}
+  validates :price, numericality: {only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
 
 
