e13a0cd707bc1e6bf4c28640d3826bec6f2ea4ab
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-19 16:32:33 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController  
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update,:destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -38,6 +38,14 @@ def update
     render :edit
   end
   
+end
+
+def destroy
+  if @item.destroy
+    redirect_to root_path
+  else
+    render :show
+  end 
 end
 
 
