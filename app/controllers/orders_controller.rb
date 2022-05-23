dd86927385cd2f19035e2878013b025cff6f1880
junta <79247735+junoota0115@users.noreply.github.com>
2022-05-23 14:53:09 +0900

    formオブジェクト、バリデーション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,4 +5,21 @@ class OrdersController < ApplicationController
   end
 
 
+  def create
+    @item = Item.find(params[:item_id])
+    @order_shipping_add = OrderShippingAdd.new(order_params)
+    if @order_shipping_add.valid?
+      @order_shipping_add.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+private
+
+  def order_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building,:phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
 end
---
+++ app/models/order_shipping_add.rb
@@ -0,0 +1,25 @@
+class OrderShippingAdd
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :order_id,:postal_code, :prefecture_id, :city, :address, :building,:phone_number,:token
+
+
+with_options presence: true do
+  # orderモデルのバリデーション
+  validates :user_id
+  validates :item_id
+  # paymentモデルのバリデーション
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: '正しく入力してください' }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :city
+  validates :address
+  validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: '正しく入力してください' }
+  # トークンのバリデーション
+  validates :token
+end
+
+  
+  def save
+    order = Order.create( user_id: user_id,item_id: item_id)
+    ShippingAdd.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number:phone_number)
+  end
+end
